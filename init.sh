910163adeff3a163549f57211ae40129b1a508b0
Alex Eftimiades <alexeftimiades@gmail.com>
2017-08-02 03:16:34 -0400

    git push current

---
+++ init.sh
@@ -0,0 +1,24 @@
+# Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# pip in brew's python
+brew install python3 wget
+wget bootstrap.pypa.io/get-pip.py
+/usr/local/bin/python3 get-pip.py
+rm get-pip.py
+
+# virtualenv installed with homebrew's python so sudo isn't needed
+pip install virtualenv
+pip3 install virtualenv
+
+# oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# iterm2
+brew cask install iterm2
+
+# neovim
+brew install neovim
+
+# hub
+brew install hub
