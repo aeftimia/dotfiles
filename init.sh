00ea07f3ed62d12c34827c849a3f7df62d2807a3
Alex Eftimiades <alexeftimiades@gmail.com>
2018-05-30 12:51:34 -0400

    - install and link emacs - kill the fish

--- init.sh
+++ init.sh
@@ -17,30 +17,6 @@ cp /usr/local/etc/fonts/fonts.conf.bak /usr/local/etc/fonts/fonts.conf
 git clone https://github.com/powerline/fonts.git
 ./fonts/install.sh
 
-# fish
-brew install fish
-echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
-chsh -s /usr/local/bin/fish
-
-# fisher
-curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisherman
-fisher up
-fisher ls-remote
-fisher z
-fisher edc/bass
-fisher cprintf
-fisher docker-completion
-brew install fzf; fisher fzf
-brew install grc; fisher grc
-
-# fish theme
-brew install --with-default-names gnu-sed
-fisher omf/theme-robbyrussel
-
-# keybindings
-set -U robbyrussel_nogreeting
-set -U fish_key_bindings fish_vi_key_bindings
-
 # iterm2
 brew cask install iterm2
 
